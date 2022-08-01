9ff39cab4f02155e991fc627d169f4b005c78704
Arcesio <jav8319@hotmail.com>
2022-08-01 14:54:00 -0400

    client files updated

---
+++ client/client/src/App.js
@@ -0,0 +1,57 @@
+
+import {
+  ApolloClient,
+  InMemoryCache,
+   ApolloProvider
+} from '@apollo/client';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+import Navbar from './components/Navbar';
+
+// Construct our main GraphQL API endpoint
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem("id_token");
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+  
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
+
+
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Routes>
+            <Route 
+              path='/' 
+              element={<SearchBooks />} 
+            />
+            <Route 
+              path='/saved' 
+              element={<SavedBooks />} 
+            />
+            <Route 
+              path='*'
+              element={<h1 className='display-2'>Wrong page!</h1>}
+            />
+          </Routes>
+        </>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
